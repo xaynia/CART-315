d5810bb8faa5a8a301f2e9450bb97a0651a3b76c
xaynia <158102890+xaynia@users.noreply.github.com>
2025-02-12 15:59:52 -0500

    Added Spawner Script
    Spawner script spawns FallingSphere prefab

--- Projects/ExplorationPrototype3/Assets/Scripts/Spawner.cs
+++ Projects/ExplorationPrototype3/Assets/Scripts/Spawner.cs
@@ -3,5 +3,19 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject fallingSpherePrefab;
+    [SerializeField] private Vector3 spawnPosition = new Vector3(0, 5f, 0f);
+
+    private void Start()
+    {
+        if (fallingSpherePrefab != null)
+        {
+            // instantiate the FallingSphere at spawnPosition
+            Instantiate(fallingSpherePrefab, spawnPosition, Quaternion.identity);
+        }
+        else
+        {
+            Debug.LogWarning("No falling sphere prefab assigned!");
+        }
+    }
 }
